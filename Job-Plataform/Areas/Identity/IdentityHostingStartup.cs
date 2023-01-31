2f33763c071544273e6f04fb968123d7eedfe411
Jefferson <jefferson-67@live.com>
2023-01-31 17:02:40 -0300

    it adds style to the login page

---
+++ Job-Plataform/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Job_Plataform.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Job_Plataform.Areas.Identity.IdentityHostingStartup))]
+namespace Job_Plataform.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
